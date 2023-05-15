365854fb9f7432130a318c8ca997e77bffc6c557
Camilo Castro <camiloandres.castro@globant.com>
2023-05-15 12:16:06 -0500

    Agregar componente para cuerpo y categorias

---
+++ FrontEnd/digitalbooking/src/components/Categories/Categories.jsx
@@ -0,0 +1,14 @@
+import CategoryCard from "../CategoryCard/CategoryCard";
+
+
+const Categories = () => {
+    return( 
+      <section className="categories-container">
+        <h3>Buscar por tipo de alojamiento --</h3>
+        <CategoryCard />
+        <CategoryCard />
+      </section>
+    );
+};
+
+export default Categories
--- FrontEnd/digitalbooking/src/pages/Home.jsx
+++ FrontEnd/digitalbooking/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import SearchBar from "../components/SearchBar/SearchBar"
+import Categories from "../components/categories/categories"
 
 const Home = () => {
   return (
     <main>
       <SearchBar />
+      <Categories />
 
     </main>
   )
