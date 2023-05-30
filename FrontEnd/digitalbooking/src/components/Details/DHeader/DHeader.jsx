538636e8488ba2b92ccbd0e6bc52667760b45dcb
Omar-Lamprea <lampreaomar@gmail.com>
2023-05-30 10:34:57 -0500

    ancho maximo para contenedores de detalles

--- FrontEnd/digitalbooking/src/components/Details/DHeader/DHeader.jsx
+++ FrontEnd/digitalbooking/src/components/Details/DHeader/DHeader.jsx
@@ -6,38 +6,42 @@ import { useNavigate } from "react-router-dom"
 
 const DHeader = ({data}) => {
   const navigate = useNavigate()
+
   const scoreStars = () =>{
     const stars = []
     for (let i = 1; i <= 5; i++) {
-      if(data.score >= i){
-        stars.push(<FontAwesomeIcon icon={faStar}  key={"star-" + i}/>)
-      }else{
-        stars.push(<FontAwesomeIcon icon={faStar} key={"star-" + i} style={{color:"gray"}}/>)
-      }
+      data.score >= i
+        ? stars.push(<FontAwesomeIcon icon={faStar}  key={"star-" + i}/>)
+        : stars.push(<FontAwesomeIcon icon={faStar} key={"star-" + i} style={{color:"gray"}}/>)
     }
     return stars
   }
+
   return (
     <section className="DHeader">
       <div className="conteiner-header blue">
-        <div className="title">
-          <p>{data.category}</p>
-          <h3>{data.name}</h3>
-        </div>
-        <div className="back" onClick={() => navigate('/')}>
-          <FontAwesomeIcon icon={faAngleLeft} />
+        <div className="col-header">
+          <div className="title">
+            <p>{data.category}</p>
+            <h3>{data.name}</h3>
+          </div>
+          <div className="back" onClick={() => navigate('/')}>
+            <FontAwesomeIcon icon={faAngleLeft} />
+          </div>
         </div>
       </div>
       <div className="conteiner-header gray">
-        <div className="location">
-          <FontAwesomeIcon icon={faLocationDot} />
-          <span>{data.city}</span>
-        </div>
-        <div className="score">
-          <div className="stars">
-            {scoreStars()}
+        <div className="col-header">
+          <div className="location">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <span>{data.city}</span>
           </div>
-          <span className="number">{data.score}</span>
+          <div className="score">
+            <div className="stars">
+              {scoreStars()}
+            </div>
+            <span className="number">{data.score}</span>
+          </div>
         </div>
       </div>
     </section>
