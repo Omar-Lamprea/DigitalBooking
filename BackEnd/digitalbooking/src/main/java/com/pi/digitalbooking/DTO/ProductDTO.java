a7e0b8ebf65bcfdd01df1364ec8851b36efd96a9
Gabriel Marquez <marquez122@outlook.com>
2023-05-29 22:20:49 -0500

    debido a un downgrade jakarta vuelve a llamarse javax

--- BackEnd/digitalbooking/src/main/java/com/pi/digitalbooking/DTO/ProductDTO.java
+++ BackEnd/digitalbooking/src/main/java/com/pi/digitalbooking/DTO/ProductDTO.java
@@ -1,6 +1,5 @@
 package com.pi.digitalbooking.DTO;
 
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- BackEnd/digitalbooking/src/main/java/com/pi/digitalbooking/controllers/ProductController.java
+++ BackEnd/digitalbooking/src/main/java/com/pi/digitalbooking/controllers/ProductController.java
@@ -18,7 +18,7 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-import jakarta.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.MultipartConfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
