f017d52fa5f2f9215ce5e9a6fc34eac27a538be0
Omar-Lamprea <lampreaomar@gmail.com>
2023-06-08 10:58:37 -0500

    se agrega libreria de para manejo de fechas en calendario doble

--- FrontEnd/digitalbooking/src/utils/constants.js
+++ FrontEnd/digitalbooking/src/utils/constants.js
@@ -1,6 +1,7 @@
 export const GLOBAL_API = {
   //products
-  urlBase: 'http://18.218.175.122:8080/digital-booking',
+  urlBase: 'http://3.138.189.133:8080/digital-booking',
+  // urlBase: 'http://localhost:8080/digital-booking',
   productsAll: "/product/all",
   product: "/product",
 
