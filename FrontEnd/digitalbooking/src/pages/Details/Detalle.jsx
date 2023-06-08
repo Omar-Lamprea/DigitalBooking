8d360ca3181792c5d8c63e2804ee359df2e0f4b6
Omar-Lamprea <lampreaomar@gmail.com>
2023-06-08 12:28:42 -0500

    bug de amenities arreglado

--- FrontEnd/digitalbooking/src/pages/Details/Detalle.jsx
+++ FrontEnd/digitalbooking/src/pages/Details/Detalle.jsx
@@ -1,11 +1,11 @@
 //import React from 'react'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faKitchenSet, faCar, faTv, faPersonSwimming, faFan, faWifi, faPaw}  from "@fortawesome/free-solid-svg-icons";
+
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useContextGlobal } from "../../context/global.context";
 import DHeader from "../../components/Details/DHeader/DHeader";
 import DContainerImages from "../../components/Details/DCarousel/DContainerImages";
+import DDescription from "../../components/Details/DDescription/DDescription";
 
 const Detalle = () => {
   const {id} = useParams()
@@ -21,12 +21,7 @@ const Detalle = () => {
   }, [state, id])
 
 
-  const descriptionNomalized = () =>{
-    const text = lodging.description 
-      ? lodging.description.replaceAll('\n', "<br />") 
-      : ""
-    return { __html: text }
-  }
+  
 
   return (
     <div className="detalle">
@@ -42,54 +37,14 @@ const Detalle = () => {
           }/>
           <DContainerImages images={lodging.images}/>
 
-          <section className="info-container"> 
-            <div className="description_container">
-            <div className="description-text">
-            <h3>{lodging.name} - {lodging.city}</h3>
-            <hr />           
-            <p className="mt-3 mt-lg-0 text-start w-100" dangerouslySetInnerHTML={descriptionNomalized()}></p>
-            </div>
-            <div className="container_icons d-flex flex-column">
-              <span>
-                <h3>¿Qué ofrece este lugar?</h3>
-                <hr />
-                <ul className="column-list">
-                  <li>
-                    <FontAwesomeIcon icon={faKitchenSet}/>
-                    <span>Cocina</span>
-                  </li>
-                  <li>
-                    <FontAwesomeIcon icon={faCar}/>
-                    <span>Estacionamiento gratuito</span>
-                  </li>
-                  <li>
-                    <FontAwesomeIcon icon={faTv}/>
-                    <span>Televisor</span>
-                  </li>
-                  <li>
-                    <FontAwesomeIcon icon={faPersonSwimming}/>
-                    <span>Pileta</span>
-                  </li>
-                  <li>
-                    <FontAwesomeIcon icon={faFan}/>
-                    <span>Aire acondicionado</span>
-                  </li>
-                  <li>
-                    <FontAwesomeIcon icon={faWifi}/>
-                    <span>Wifi</span>
-                  </li>
-                  <li>
-                    <FontAwesomeIcon icon={faPaw}/>
-                    <span>Apto mascotas</span>
-                  </li>
-                </ul>
-              </span>
-              {state?.user?.data &&
-                <button> Reserva ahora! </button>
-              }
-            </div>
-            </div>
-          </section>
+          <DDescription lodging={
+            {
+              name: lodging.name, 
+              city: lodging.city,
+              description: lodging.description,
+              amenities: lodging.amenities
+            }
+          }/>
         </>
         : <div className="errorProduct">
             <h5>No encontramos ningun producto con el id {id}</h5>
