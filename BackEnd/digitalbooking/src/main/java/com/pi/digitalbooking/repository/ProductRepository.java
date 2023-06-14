f22c93edf73f4f94564543db58b0746aff41f263
Gabriel Marquez <marquez122@outlook.com>
2023-06-14 10:50:05 -0500

    Custom query method to get products in a city that do not have a booking

--- BackEnd/digitalbooking/src/main/java/com/pi/digitalbooking/repository/ProductRepository.java
+++ BackEnd/digitalbooking/src/main/java/com/pi/digitalbooking/repository/ProductRepository.java
@@ -4,8 +4,11 @@ import com.pi.digitalbooking.enums.ProductStatus;
 import com.pi.digitalbooking.models.City;
 import com.pi.digitalbooking.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -14,6 +17,15 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     List<Product> findAllByStatus(ProductStatus status);
     Product findByCodeProductAndStatus(Integer codeProduct, ProductStatus status);
     List<Product> findByCategoryCategoryId (int id);
-    List<Product> findByCityStartingWith(String city);
     List<Product> findByCityAndStatus(City city, ProductStatus status);
+
+    @Query("SELECT p FROM Product p WHERE p.city.name = :city AND p.status = :status AND p NOT IN " +
+            "(SELECT b.product FROM BookingEntity b WHERE b.checkInDate <= :checkOutDate " +
+            "AND b.checkOutDate >= :checkInDate AND b.status = :status)")
+    List<Product> findActiveProductsWithoutBooking(
+            @Param("city") String cityName,
+            @Param("checkInDate") LocalDate checkInDate,
+            @Param("checkOutDate") LocalDate checkOutDate,
+            @Param("status") ProductStatus status);
+
 }
