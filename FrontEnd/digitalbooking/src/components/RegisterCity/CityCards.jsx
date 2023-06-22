524175dd090383103ab15675228d1c03e33380b4
Omar-Lamprea <lampreaomar@gmail.com>
2023-06-22 17:38:52 -0500

    template para actualizar ciudades

--- FrontEnd/digitalbooking/src/components/RegisterCity/CityCards.jsx
+++ FrontEnd/digitalbooking/src/components/RegisterCity/CityCards.jsx
@@ -1,7 +1,30 @@
 import PropTypes from 'prop-types';
 import ModalDeleteCity from '../Modals/ModalDeleteCity';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
+import { useState } from 'react';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 const CityCards = (props) => {
+  const [changeInput, setChangeInput] =useState(false)
+  const [cityName, setCityName] = useState(props.data.name)
+  const [cityUpdated, setCityUpdated] = useState(props.data.name)
+
+  const handleChange = (e) => {
+    setCityName(e.target.value)
+  }
+
+  const handleSubmit = () =>{
+    if(cityName === cityUpdated){
+      setChangeInput(false)
+    }else{
+      console.log('updating name to: ', cityName);
+      setCityUpdated(cityName)
+      setChangeInput(false)
+    }
+  }
+
+
   return (
     <section className='city'>
       <div className="city-data">
@@ -9,10 +32,29 @@ const CityCards = (props) => {
           <span>{props.data.country.name}</span>
         </p>
         <p>Ciudad: <br />
-          <span>{props.data.name}</span>
+          {!changeInput
+            ? <span>{cityUpdated}</span>
+            : <input 
+                type="text" 
+                id='input-city'
+                defaultValue={cityUpdated}
+                onChange={handleChange}
+                onKeyUp={(e)=> {
+                  if(e.key === 'Enter') handleSubmit(cityName)
+                }}
+              />
+          }
+          
         </p>
       </div>
       <div className="city-actions">
+        <div className="update">
+          {!changeInput
+            ? <FontAwesomeIcon icon={faPenToSquare} onClick={() => setChangeInput(true)}/>
+            : <FontAwesomeIcon icon={faCheck} onClick={handleSubmit}/>
+          }
+        </div>
+
         <div className="delete">
           <ModalDeleteCity deleteId={props.deleteId} id={props.data.cityId} cityName={props.data.name}/>
         </div>
