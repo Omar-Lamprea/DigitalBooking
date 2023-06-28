fe0c57937da14b16fb42c56afa6aac2bdf64bf77
npolomoreno <npolomoreno@gmail.com>
2023-06-28 11:23:03 -0500

    maqueta success reservation

--- FrontEnd/digitalbooking/src/pages/Bookings/SuccessBooking.jsx
+++ FrontEnd/digitalbooking/src/pages/Bookings/SuccessBooking.jsx
@@ -1,8 +1,18 @@
 
-
+import './SuccessBooking.scss'
+import {BsFillPatchCheckFill} from 'react-icons/bs'
 const SuccessBooking = () => {
+
+
   return (
-    <div>SuccessBooking</div>
+    <div className='SuccesBooking'>
+      <div className="SuccesBooking-main">  
+        <BsFillPatchCheckFill className='check-icon'/>
+        <h3>¡Muchas gracias!</h3>
+        <p>Su reserva se ha realizado con éxito</p>
+        <button className="custom-button">Ok</button>
+      </div>
+    </div>
   )
 }
 
