18362bc306a45ec3826a3cf71b91ad51947d9137
Omar-Lamprea <lampreaomar@gmail.com>
2023-06-27 22:47:32 -0500

    validation name

--- FrontEnd/digitalbooking/src/components/RegisterCity/CityCards.jsx
+++ FrontEnd/digitalbooking/src/components/RegisterCity/CityCards.jsx
@@ -7,10 +7,11 @@ import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { useContextGlobal } from '../../context/global.context';
 
 const CityCards = (props) => {
+  const {data, deleteId} = props
   const {state} = useContextGlobal()
   const [changeInput, setChangeInput] =useState(false)
-  const [cityName, setCityName] = useState(props.data.name)
-  const [cityUpdated, setCityUpdated] = useState(props.data.name)
+  const [cityName, setCityName] = useState(data.name)
+  const [cityUpdated, setCityUpdated] = useState(data.name)
   const[errorUpdate, setErrorUpdate] = useState(false)
 
   const handleChange = (e) => {
@@ -25,7 +26,7 @@ const CityCards = (props) => {
         const response = await fetch(
           state.URL_API.urlBase + 
           state.URL_API.city + 
-          `/?idCity=${props.data.cityId}&nameToUpdate=${cityName}`,{
+          `/?idCity=${data.cityId}&nameToUpdate=${cityName}`,{
             method: "PUT",
             headers: {
               Authorization: `Bearer ${state.user.token}`
@@ -50,7 +51,7 @@ const CityCards = (props) => {
     <section className='city'>
       <div className="city-data">
         <p>Pais: <br />
-          <span>{props.data.country.name}</span>
+          <span>{data?.country?.name}</span>
         </p>
         <p>Ciudad: <br />
           {!changeInput
@@ -86,7 +87,7 @@ const CityCards = (props) => {
         </div>
 
         <div className="delete">
-          <ModalDeleteCity deleteId={props.deleteId} id={props.data.cityId} cityName={props.data.name}/>
+          <ModalDeleteCity deleteId={deleteId} id={data.cityId} cityName={data.name}/>
         </div>
       </div>
     </section>
