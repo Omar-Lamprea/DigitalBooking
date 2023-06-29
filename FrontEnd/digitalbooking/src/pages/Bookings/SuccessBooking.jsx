b6d7f1484a6115610964e18d18e12827d0093562
Omar-Lamprea <lampreaomar@gmail.com>
2023-06-28 22:56:45 -0500

    merge with success bookings

--- FrontEnd/digitalbooking/src/pages/Bookings/SuccessBooking.jsx
+++ FrontEnd/digitalbooking/src/pages/Bookings/SuccessBooking.jsx
@@ -3,11 +3,7 @@ import { Link } from 'react-router-dom'
 import './SuccessBooking.scss'
 import {BsFillPatchCheckFill} from 'react-icons/bs'
 
-
-
 const SuccessBooking = () => {
-
-
   return (
     <div className='SuccesBooking'>
       <div className="SuccesBooking-main">  
